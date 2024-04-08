0bb879e6747eb8a9ba335dd89f776dae6972ca07
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-04-08 10:14:06 +0700

    feat: api-function

--- src/utils/api.js
+++ src/utils/api.js
@@ -40,3 +40,90 @@ async function login({ email, password }) {
 
   return { error: false, data: responseJson.data };
 }
+
+async function register({ name, email, password }) {
+  const response = await fetch(`${BASE_URL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, email, password }),
+  });
+
+  const responseJson = await response.json();
+
+  if (responseJson.status !== "success") {
+    alert(responseJson.message);
+    return { error: true };
+  }
+
+  return { error: false };
+}
+
+async function getUserLogged() {
+  const response = await fetchWithToken(`${BASE_URL}/users/me`);
+  const responseJson = await response.json();
+
+  if (responseJson.status !== "success") {
+    return { error: true, data: null };
+  }
+
+  return { error: false, data: responseJson.data };
+}
+
+async function addContact({ name, tag }) {
+  const response = await fetchWithToken(`${BASE_URL}/contacts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, tag }),
+  });
+
+  const responseJson = await response.json();
+
+  if (responseJson.status !== "success") {
+    alert(responseJson.message);
+    return { error: true };
+  }
+
+  return { error: false };
+}
+
+async function getContacts() {
+  const response = await fetchWithToken(`${BASE_URL}/contacts`);
+  const responseJson = await response.json();
+
+  if (responseJson.status !== "success") {
+    alert(responseJson.message);
+    return { error: true, data: null };
+  }
+
+  return { error: false, data: responseJson.data };
+}
+
+async function deleteContact(id) {
+  const response = await fetchWithToken(`${BASE_URL}/contacts/${id}`, {
+    method: "DELETE",
+  });
+
+  const responseJson = await response.json();
+
+  if (responseJson.status !== "success") {
+    alert(responseJson.message);
+    return { error: true };
+  }
+
+  return { error: false };
+}
+
+export {
+  getAccessToken,
+  putAccessToken,
+  login,
+  register,
+  getUserLogged,
+  addContact,
+  getContacts,
+  deleteContact,
+};
